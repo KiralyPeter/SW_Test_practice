cb83a9c3408b5fbf4f3a5064f25dfd58b7a68aaf
Peter Kiraly <peter.kiral@gmail.com>
2023-05-02 19:59:37 +0200

    window is ready

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,14 +1,35 @@
+import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 
 public class MainFrame extends JFrame{
 
+    InputPanel tomegPanel;
+    InputPanel magassagPanel;
+    InputPanel indexPanel;
+
     public MainFrame() {
+        this.initComponent();
+        this.addComponent();
         this.setFrame();
     }
 
+    private void initComponent(){
+        this.tomegPanel = new InputPanel("Tömeg");
+        this.magassagPanel = new InputPanel("Magasság");
+        this.indexPanel = new InputPanel("Testtömegindex");
+    }
+
+    private void addComponent(){
+        this.add(this.tomegPanel);
+        this.add(this.magassagPanel);
+        this.add(this.indexPanel);
+    }
+
     private void setFrame() {
+        this.setLayout(new BoxLayout(this.getContentPane(), BoxLayout.PAGE_AXIS));
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(400, 300);
+        this.pack();
+        // this.setSize(300, 200);
         this.setVisible(true);
     }
     
