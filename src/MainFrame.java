5060c8871f0343e6de23dcb2a4a38b8ede0bf5dd
Peter Kiraly <peter.kiral@gmail.com>
2023-05-09 17:41:47 +0200

    mukodik a szamitas

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,4 +1,5 @@
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -8,18 +9,22 @@ public class MainFrame extends JFrame{
     InputPanel magassagPanel;
     InputPanel indexPanel;
     JPanel buttoPanel;
+    JButton calcButton;
+
 
     public MainFrame() {
         this.initComponent();
         this.addComponent();
+        this.hadleEvent();
         this.setFrame();
     }
 
     private void initComponent(){
-        this.tomegPanel = new InputPanel("Tömeg");
-        this.magassagPanel = new InputPanel("Magasság");
+        this.tomegPanel = new InputPanel("Tömeg (Kg)");
+        this.magassagPanel = new InputPanel("Magasság (m)");
         this.buttoPanel = new JPanel();
         this.indexPanel = new InputPanel("Testtömegindex");
+        this.calcButton = new JButton("Számít");
     }
 
     private void addComponent(){
@@ -27,6 +32,28 @@ public class MainFrame extends JFrame{
         this.add(this.magassagPanel);
         this.add(this.buttoPanel);
         this.add(this.indexPanel);
+        this.buttoPanel.add(this.calcButton);
+    }
+
+    private void hadleEvent(){
+        this.calcButton.addActionListener( e -> {
+            startCalc();
+        }); //névtelen függvény
+    }
+
+    private void startCalc(){
+        String tomegStr = this.tomegPanel.getValue();
+        double tomeg = Double.parseDouble(tomegStr);
+        String magassagStr = this.magassagPanel.getValue();
+        double magassag = Double.parseDouble(magassagStr);
+
+        Double testtomegIndex = this.calcBodyIndex(tomeg, magassag);
+        this.indexPanel.setValue(testtomegIndex.toString());
+    }
+
+    public double calcBodyIndex(double weight, double height){
+
+        return weight / Math.pow(height, 2);
     }
 
     private void setFrame() {
